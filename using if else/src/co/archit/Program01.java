26e8b2f727d61adb700c379dc2b172f629f66435
08architMBD <architsaxena@gmail.com>
2022-02-25 17:44:47 +0530

    yes

---
+++ using if else/src/co/archit/Program01.java
@@ -0,0 +1,17 @@
+package co.archit;
+
+public class Program01 {
+	public static void main(String[] args) {
+		int num1, num2;
+		num1=80;
+		num2=260;
+		if(num1>num2) {
+			System.out.println("num2 is greater");
+		}
+		else {
+			System.out.println("num2 is not greater");
+		}
+		System.out.println("end of program");
+	}
+
+}
---
+++ using if else/src/co/archit/Program02.java
@@ -0,0 +1,16 @@
+package co.archit;
+
+public class Program02 {
+	
+	public static void main(String[] args) {
+		int year=2100;
+		if((year%4==0  && year%100 !=0)|| year%400==0) {
+			System.out.println("Year"+year+"is leap year");
+		}else {
+			System.out.println("Year"+year+"is not a leap year");
+			
+		}
+	
+}
+
+}
